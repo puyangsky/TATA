17d45921ec398da068db666085577efbee052a89
puyangsky <puyangsky@163.com>
2015-12-01 00:33:55 +0800

    Added left sliding menu and it now looks very cool.

--- leanchat/src/main/java/com/avoscloud/chat/activity/MainActivity.java
+++ leanchat/src/main/java/com/avoscloud/chat/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.widget.Button;
 
@@ -35,6 +36,7 @@ import com.baidu.location.LocationClientOption;
 import de.greenrobot.event.EventBus;
 
 import com.avoscloud.chat.activity.PublishActivity;
+import com.nineoldandroids.view.ViewHelper;
 
 /**
  * Created by lzw on 14-9-17.
@@ -64,7 +66,7 @@ public class MainActivity extends BaseActivity {
   ProfileFragment profileFragment;  //没有publish Fragment
   Button[] tabs;
   View recentTips, contactTips;
-
+  DrawerLayout mDrawerLayout;
   public static void goMainActivityFromActivity(Activity fromActivity) {
     EventBus eventBus = EventBus.getDefault();
     eventBus.post(new LoginFinishEvent());
@@ -85,6 +87,7 @@ public class MainActivity extends BaseActivity {
     findView();
     init();
 
+    initDrawer();
     //mySpaceBtn.performClick();
     //contactBtn.performClick();
     conversationBtn.performClick();
@@ -257,4 +260,63 @@ public class MainActivity extends BaseActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
     }
+  private void initDrawer()
+  {
+    mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    mDrawerLayout.setDrawerListener(new DrawerLayout.DrawerListener()
+    {
+      @Override
+      public void onDrawerStateChanged(int newState)
+      {
+      }
+
+      @Override
+      public void onDrawerSlide(View drawerView, float slideOffset)
+      {
+        View mContent = mDrawerLayout.getChildAt(0);
+        View mMenu = drawerView;
+        float scale = 1 - slideOffset;
+        float rightScale = 0.8f + scale * 0.2f;
+
+        if (drawerView.getTag().equals("LEFT"))
+        {
+
+          float leftScale = 1 - 0.3f * scale;
+
+          ViewHelper.setScaleX(mMenu, leftScale);
+          ViewHelper.setScaleY(mMenu, leftScale);
+          ViewHelper.setAlpha(mMenu, 0.6f + 0.4f * (1 - scale));
+          ViewHelper.setTranslationX(mContent,
+                  mMenu.getMeasuredWidth() * (1 - scale));
+          ViewHelper.setPivotX(mContent, 0);
+          ViewHelper.setPivotY(mContent,
+                  mContent.getMeasuredHeight() / 2);
+          mContent.invalidate();
+          ViewHelper.setScaleX(mContent, rightScale);
+          ViewHelper.setScaleY(mContent, rightScale);
+        } else
+        {
+          ViewHelper.setTranslationX(mContent,
+                  -mMenu.getMeasuredWidth() * slideOffset);
+          ViewHelper.setPivotX(mContent, mContent.getMeasuredWidth());
+          ViewHelper.setPivotY(mContent,
+                  mContent.getMeasuredHeight() / 2);
+          mContent.invalidate();
+          ViewHelper.setScaleX(mContent, rightScale);
+          ViewHelper.setScaleY(mContent, rightScale);
+        }
+
+      }
+
+      @Override
+      public void onDrawerOpened(View drawerView)
+      {
+      }
+
+      @Override
+      public void onDrawerClosed(View drawerView)
+      {
+      }
+    });
+  }
 }
