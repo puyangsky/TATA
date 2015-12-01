4c3de44eb8e5f18a9bce773fef3e40d0811bcc22
lihongqiang <415200973@qq.com>
2015-12-01 19:23:54 +0800

    重构publishActivity

--- leanchat/src/main/java/com/avoscloud/chat/activity/PublishActivity.java
+++ leanchat/src/main/java/com/avoscloud/chat/activity/PublishActivity.java
@@ -1,17 +1,31 @@
 package com.avoscloud.chat.activity;
 
+import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -21,8 +35,13 @@ import com.avos.avoscloud.AVUser;
 import com.avos.avoscloud.SaveCallback;
 import com.avoscloud.chat.R;
 import com.avoscloud.chat.model.Moment;
+import com.avoscloud.chat.util.Bimp;
+import com.avoscloud.chat.util.FileUtils;
+import com.avoscloud.chat.util.ImageItem;
 import com.avoscloud.chat.util.PathUtils;
 import com.avoscloud.chat.util.PhotoUtils;
+import com.avoscloud.chat.util.PublicWay;
+import com.avoscloud.chat.util.Res;
 import com.avoscloud.leanchatlib.model.LeanchatUser;
 
 import java.io.File;
@@ -35,7 +54,11 @@ import butterknife.InjectView;
 import butterknife.OnClick;
 
 import android.util.Log;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 
 import javax.security.auth.login.LoginException;
 
@@ -43,6 +66,13 @@ public class PublishActivity extends Activity {
 
     private static final int IMAGE_PICK_REQUEST = 1;
     private static final int CROP_REQUEST = 2;
+    private static final int TAKE_PICTURE = 3;
+
+    //当前的activity为最开始的parent
+    private View parentView;
+
+    //添加图片的按钮图片
+    public static Bitmap bimap ;
 
     //选择提交的图片
     private Bitmap bitmap = null;
@@ -52,6 +82,15 @@ public class PublishActivity extends Activity {
 
     @InjectView(R.id.activity_publish_btn)
     public Button publish_btn;
+
+    //弹出框
+    private PopupWindow pop;
+    //弹出框线性布局
+    private LinearLayout ll_popup;
+
+    private GridAdapter adapter;
+
+    private GridView noScrollgridview;
 
     @OnClick(R.id.activity_publish_btn)
     public void onPublish_Btn_Click() {
@@ -64,35 +103,55 @@ public class PublishActivity extends Activity {
     @InjectView(R.id.activity_publish_text)
     public EditText publish_text;
 
-    @InjectView(R.id.publish_addbutton_view)
-    public ImageView imageView;
+//    @InjectView(R.id.publish_addbutton_view)
+//    public ImageView imageView;
 
-    @OnClick(R.id.publish_addbutton_view)
-    public void onAvatarClick() {
-        Intent intent = new Intent(Intent.ACTION_PICK, null);
-        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-        startActivityForResult(intent, IMAGE_PICK_REQUEST);
-    }
+//    @OnClick(R.id.publish_addbutton_view)
+//    public void onAvatarClick() {
+//        Intent intent = new Intent(Intent.ACTION_PICK, null);
+//        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+//        startActivityForResult(intent, IMAGE_PICK_REQUEST);
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_publish);
+        Res.init(this);
+        bimap = BitmapFactory.decodeResource(
+                getResources(),
+                R.drawable.icon_addpic_unfocused);
+        PublicWay.activityList.add(this);       //结束是所有的activity一起结束
+        parentView = getLayoutInflater().inflate(R.layout.activity_publish, null);
+        setContentView(parentView);
         ButterKnife.inject(this);
+        InitPopWindow();
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == IMAGE_PICK_REQUEST) {
-                Uri uri = data.getData();
-                picPath = PhotoUtils.getImageAbsolutePath(this, uri);
-                Log.e("picPath", picPath);
-                bitmap = PhotoUtils.getImageThumbnail(picPath, 300, 300);  //压缩图片,压缩比需要调整
-                Log.e("compress bitmap", "done");
-                imageView.setImageBitmap(bitmap);       //更新本地图片，这里没有更改压缩图片，下一步更新
-            }
+        switch (requestCode) {
+//            case RESULT_OK:
+//                if (requestCode == IMAGE_PICK_REQUEST) {
+//                    Uri uri = data.getData();
+//                    picPath = PhotoUtils.getImageAbsolutePath(this, uri);
+//                    Log.e("picPath", picPath);
+//                    bitmap = PhotoUtils.getImageThumbnail(picPath, 300, 300);  //压缩图片,压缩比需要调整
+//                    Log.e("compress bitmap", "done");
+//                    imageView.setImageBitmap(bitmap);       //更新本地图片，这里没有更改压缩图片，下一步更新
+//                }
+            case TAKE_PICTURE:
+                if (Bimp.tempSelectBitmap.size() < 9 && resultCode == RESULT_OK) {
+
+                    String fileName = String.valueOf(System.currentTimeMillis());
+                    Bitmap bm = (Bitmap) data.getExtras().get("data");
+                    FileUtils.saveBitmap(bm, fileName);
+
+                    ImageItem takePhoto = new ImageItem();
+                    takePhoto.setBitmap(bm);
+                    Bimp.tempSelectBitmap.add(takePhoto);
+                }
+                break;
         }
     }
 
@@ -216,6 +275,218 @@ public class PublishActivity extends Activity {
             e.printStackTrace();
         }
         return file;
+    }
+
+    /**
+     * 弹出框初始化
+     */
+    public void InitPopWindow() {
+
+        pop = new PopupWindow(PublishActivity.this);
+
+        View view = getLayoutInflater().inflate(R.layout.item_popupwindows, null);
+
+        ll_popup = (LinearLayout) view.findViewById(R.id.ll_popup);
+
+        pop.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+        pop.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        pop.setBackgroundDrawable(new BitmapDrawable());
+        pop.setFocusable(true);
+        pop.setOutsideTouchable(true);
+        pop.setContentView(view);
+
+        RelativeLayout parent = (RelativeLayout) view.findViewById(R.id.parent);
+        Button bt1 = (Button) view
+                .findViewById(R.id.item_popupwindows_camera);
+        Button bt2 = (Button) view
+                .findViewById(R.id.item_popupwindows_Photo);
+        Button bt3 = (Button) view
+                .findViewById(R.id.item_popupwindows_cancel);
+        parent.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                pop.dismiss();
+                ll_popup.clearAnimation();
+            }
+        });
+        bt1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                photo();
+                pop.dismiss();
+                ll_popup.clearAnimation();
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(PublishActivity.this,
+                        AlbumActivity.class);
+                Log.e("bt2 intent", "start");
+                startActivity(intent);
+                Log.e("bt2 intent", "end");
+                overridePendingTransition(R.anim.activity_translate_in, R.anim.activity_translate_out);
+                pop.dismiss();
+                ll_popup.clearAnimation();
+            }
+        });
+        bt3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                pop.dismiss();
+                ll_popup.clearAnimation();
+            }
+        });
+
+        noScrollgridview = (GridView) findViewById(R.id.noScrollgridview);
+        noScrollgridview.setSelector(new ColorDrawable(Color.TRANSPARENT));
+        adapter = new GridAdapter(this);
+        adapter.update();
+        noScrollgridview.setAdapter(adapter);
+        noScrollgridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+                if (arg2 == Bimp.tempSelectBitmap.size()) {
+                    Log.i("ddddddd", "----------");
+                    ll_popup.startAnimation(AnimationUtils.loadAnimation(PublishActivity.this, R.anim.activity_translate_in));
+                    pop.showAtLocation(parentView, Gravity.BOTTOM, 0, 0);
+                } else {
+                    Intent intent = new Intent(PublishActivity.this,
+                            GalleryActivity.class);
+                    intent.putExtra("position", "1");
+                    intent.putExtra("ID", arg2);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+
+    @SuppressLint("HandlerLeak")
+    public class GridAdapter extends BaseAdapter {
+        private LayoutInflater inflater;
+        private int selectedPosition = -1;
+        private boolean shape;
+
+        public boolean isShape() {
+            return shape;
+        }
+
+        public void setShape(boolean shape) {
+            this.shape = shape;
+        }
+
+        public GridAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+        }
+
+        public void update() {
+            loading();
+        }
+
+        public int getCount() {
+            if(Bimp.tempSelectBitmap.size() == 9){
+                return 9;
+            }
+            return (Bimp.tempSelectBitmap.size() + 1);
+        }
+
+        public Object getItem(int arg0) {
+            return null;
+        }
+
+        public long getItemId(int arg0) {
+            return 0;
+        }
+
+        public void setSelectedPosition(int position) {
+            selectedPosition = position;
+        }
+
+        public int getSelectedPosition() {
+            return selectedPosition;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.item_published_grida,
+                        parent, false);
+                holder = new ViewHolder();
+                holder.image = (ImageView) convertView
+                        .findViewById(R.id.item_grida_image);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            if (position ==Bimp.tempSelectBitmap.size()) {
+                holder.image.setImageBitmap(BitmapFactory.decodeResource(
+                        getResources(), R.drawable.icon_addpic_unfocused));
+                if (position == 9) {
+                    holder.image.setVisibility(View.GONE);
+                }
+            } else {
+                holder.image.setImageBitmap(Bimp.tempSelectBitmap.get(position).getBitmap());
+            }
+
+            return convertView;
+        }
+
+        public class ViewHolder {
+            public ImageView image;
+        }
+
+        Handler handler = new Handler() {
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case 1:
+                        adapter.notifyDataSetChanged();
+                        break;
+                }
+                super.handleMessage(msg);
+            }
+        };
+
+        public void loading() {
+            new Thread(new Runnable() {
+                public void run() {
+                    while (true) {
+                        if (Bimp.max == Bimp.tempSelectBitmap.size()) {
+                            Message message = new Message();
+                            message.what = 1;
+                            handler.sendMessage(message);
+                            break;
+                        } else {
+                            Bimp.max += 1;
+                            Message message = new Message();
+                            message.what = 1;
+                            handler.sendMessage(message);
+                        }
+                    }
+                }
+            }).start();
+        }
+    }
+
+    public String getString(String s) {
+        String path = null;
+        if (s == null)
+            return "";
+        for (int i = s.length() - 1; i > 0; i++) {
+            s.charAt(i);
+        }
+        return path;
+    }
+
+    protected void onRestart() {
+        adapter.update();
+        super.onRestart();
+    }
+
+    public void photo() {
+        Intent openCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(openCameraIntent, TAKE_PICTURE);
     }
 
 }
---
+++ leanchat/src/main/java/com/avoscloud/chat/util/ImageBucket.java
@@ -0,0 +1,10 @@
+package com.avoscloud.chat.util;
+
+import java.util.List;
+
+public class ImageBucket {
+	public int count = 0;
+	public String bucketName;
+	public List<ImageItem> imageList;
+
+}
