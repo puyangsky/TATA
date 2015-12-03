dccce43c32f54f0fe5737c9b2acaa8939fc5a53d
puyangsky <puyangsky@163.com>
2015-12-03 21:49:43 +0800

    Added function to left slide menu.

--- leanchat/src/main/java/com/avoscloud/chat/fragment/MenuLeftFragment.java
+++ leanchat/src/main/java/com/avoscloud/chat/fragment/MenuLeftFragment.java
@@ -2,20 +2,38 @@ package com.avoscloud.chat.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.avoscloud.chat.R;
+import com.avoscloud.leanchatlib.controller.ChatManager;
+
 
 /**
  * Created by Administrator on 2015/11/30.
  */
 public class MenuLeftFragment extends Fragment {
-    @Nullable
+    private TextView logoutView;
+    ChatManager chatManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.layout_left_menu, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        chatManager = ChatManager.getInstance();
+        logoutView = (TextView) getView().findViewById(R.id.tv_log_out);
+        logoutView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "FUCK", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
